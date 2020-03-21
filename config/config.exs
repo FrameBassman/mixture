a7420072dfe3814e0235ce279317ae2e19e8b3aa
Vladimir Mitin <vmitin.dev@gmail.com>
2020-03-21 17:56:18 +0300

    got rid of warnings (2)

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,12 @@
 import Config
 
-import_config "#{Mix.env}.exs"
+case Mix.env() do
+       :test -> config :junit_formatter,
+                        report_file: "junit_report.xml",
+                        report_dir: "_build/",
+                        print_report_file: true,
+                        prepend_project_name?: true
+
+       :dev -> nil
+       :prod -> nil
+end
--- config/text.exs
+++
@@ -1,7 +0,0 @@
-import Config
-
-config :junit_formatter,
-  report_file: "junit_report.xml",
-  report_dir: "./_build/",
-  print_report_file: true,
-  prepend_project_name?: true
